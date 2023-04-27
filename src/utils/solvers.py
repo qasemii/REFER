bd370b1d283d68f0d8e982a7ebdc93b59c335b2d
Reza Madani <rezamadani.ai@gmail.com>
2023-04-27 12:30:39 +0100

    Update

--- src/utils/solvers.py
+++ src/utils/solvers.py
@@ -3,33 +3,6 @@ from torch import Tensor
 from typing import Callable
 
 
-def top_k_percent(attrs: Tensor, k: int, min_val: float = -1e10) -> Tensor:
-    attn_mask = attrs.bool().int()
-    num_tokens = torch.sum(attn_mask, dim=1) - 1  # don't include CLS token when computing num_tokens
-    num_highlight_tokens = torch.round(num_tokens * k / 100)
-    ones = torch.ones_like(num_highlight_tokens)
-    num_highlight_tokens = torch.maximum(num_highlight_tokens, ones).long()
-
-    attrs = attrs + (1 - attn_mask) * min_val  # ignore pad tokens when computing sorted_attrs_indices
-    attrs[:, 0] = min_val  # don't include CLS token when computing sorted_attrs_indices
-
-    expls = []
-    for i in range(len(attrs)):
-        scores, indices = torch.topk(attrs[i, :], num_highlight_tokens[i].item(), sorted=True)
-        threshold = scores[-1]
-
-        expl = (attrs[i:i + 1, :] >= threshold).float()
-        expl[:, 0] = 1  # always treat CLS token as positive token
-
-        expls.append(expl)
-
-    return torch.cat(expls)
-
-
-# #####################################################################################################
-# OLD VERSION - DOESN'T WORK (NOT DIFFERENTIABLE WITH IMLE)
-# #####################################################################################################
-#
 # def top_k_percent(attrs: Tensor, k: int, min_val: float = -1e10) -> Tensor:
 #     attn_mask = attrs.bool().int()
 #     num_tokens = torch.sum(attn_mask, dim=1) - 1  # don't include CLS token when computing num_tokens
@@ -39,12 +12,39 @@ def top_k_percent(attrs: Tensor, k: int, min_val: float = -1e10) -> Tensor:
 #
 #     attrs = attrs + (1 - attn_mask) * min_val  # ignore pad tokens when computing sorted_attrs_indices
 #     attrs[:, 0] = min_val  # don't include CLS token when computing sorted_attrs_indices
-#     sorted_attrs_indices = torch.argsort(attrs, dim=1, descending=True)
 #
-#     expl = torch.zeros_like(attn_mask).long()
+#     expls = []
 #     for i in range(len(attrs)):
-#         salient_indices = sorted_attrs_indices[i][:num_highlight_tokens[i]]
-#         expl[i, salient_indices] = 1
-#     expl[:, 0] = 1  # always treat CLS token as positive token
+#         scores, indices = torch.topk(attrs[i, :], num_highlight_tokens[i].item(), sorted=True)
+#         threshold = scores[-1]
 #
-#     return expl
+#         expl = (attrs[i:i + 1, :] >= threshold).float()
+#         expl[:, 0] = 1  # always treat CLS token as positive token
+#
+#         expls.append(expl)
+#
+#     return torch.cat(expls)
+
+
+# #####################################################################################################
+# OLD VERSION - DOESN'T WORK (NOT DIFFERENTIABLE WITH IMLE)
+# #####################################################################################################
+#
+def top_k_percent(attrs: Tensor, k: int, min_val: float = -1e10) -> Tensor:
+    attn_mask = attrs.bool().int()
+    num_tokens = torch.sum(attn_mask, dim=1) - 1  # don't include CLS token when computing num_tokens
+    num_highlight_tokens = torch.round(num_tokens * k / 100)
+    ones = torch.ones_like(num_highlight_tokens)
+    num_highlight_tokens = torch.maximum(num_highlight_tokens, ones).long()
+
+    attrs = attrs + (1 - attn_mask) * min_val  # ignore pad tokens when computing sorted_attrs_indices
+    attrs[:, 0] = min_val  # don't include CLS token when computing sorted_attrs_indices
+    sorted_attrs_indices = torch.argsort(attrs, dim=1, descending=True)
+
+    expl = torch.zeros_like(attn_mask).long()
+    for i in range(len(attrs)):
+        salient_indices = sorted_attrs_indices[i][:num_highlight_tokens[i]]
+        expl[i, salient_indices] = 1
+    expl[:, 0] = 1  # always treat CLS token as positive token
+
+    return expl.float
